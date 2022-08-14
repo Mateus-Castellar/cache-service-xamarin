b376fa4db4c6f77e31cb55ac6ac9dfe9be45e459
Mateus Castellar <mateuscastellar@outlook.com>
2022-08-13 23:45:59 -0300

    Add project files.

---
+++ CacheamentoDeDados/CacheamentoDeDados/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using CacheamentoDeDados.Models;
+using CacheamentoDeDados.Service.Cache;
+using System;
+using System.Text.Json;
+using Xamarin.Forms;
+
+namespace CacheamentoDeDados
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage() => InitializeComponent();
+
+        private async void CadastrarUsuario(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(EtrNome.Text) || string.IsNullOrEmpty(EtrEmail.Text) ||
+                string.IsNullOrEmpty(EtrSenha.Text) || string.IsNullOrEmpty(EtrSenhaConfirmacao.Text))
+            {
+                await DisplayAlert("Atenção",
+                    "Todos os campos devem ser preenchido", "Fechar");
+            }
+
+            var cacheamentoService = new CacheamentoService();
+
+            var usuario = new Usuario
+            {
+                Email = EtrEmail.Text,
+                Nome = EtrNome.Text,
+                Senha = EtrSenha.Text,
+                SenhaConfirmacao = EtrSenhaConfirmacao.Text
+            };
+
+            var usuarioJson = JsonSerializer.Serialize(usuario);
+            cacheamentoService.ArmazenarDado("usuario-cadastrado", usuarioJson);
+
+            await DisplayAlert("Sucesso",
+                "Usuario Cadastrado Com Sucesso", "Fechar");
+        }
+    }
+}
---
+++ CacheamentoDeDados/CacheamentoDeDados/Service/Cache/CacheamentoService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace CacheamentoDeDados.Service.Cache
+{
+    public class CacheamentoService
+    {
+        public async void ArmazenarDado(string chave, string valor)
+        {
+            try
+            {
+                await SecureStorage.SetAsync(chave, valor);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                throw;
+            }
+        }
+
+        public async Task<string> ObterDado(string chave)
+        {
+            try
+            {
+                return await SecureStorage.GetAsync(chave);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                throw;
+            }
+        }
+
+        public void LimparDado(string chave) => SecureStorage.Remove(chave);
+
+        public void LimparTodosDados() => SecureStorage.RemoveAll();
+    }
+}
